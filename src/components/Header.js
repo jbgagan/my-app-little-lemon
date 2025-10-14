79ef7688c65a32c42cdf31aa133d08b547b4a479
jbgagan <gagandeepjb@hotmail.com>
2025-10-14 16:44:55 +0530

    desktop version

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
+import Nav from "./Nav";
 function Header (){
     return (
         <>
         <header>
             
-        </header>
+            <Nav />
+            
+                    </header>
         </>
     );
 };
